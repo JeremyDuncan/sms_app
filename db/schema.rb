8a9d9627e270952333a00a7ed172596b38fb6848
JeremyDuncan <jduncan46@my.devry.edu>
2024-07-05 13:54:25 -0400

    added sms_logs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_05_173734) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_05_175339) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sms_logs", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "phone_number"
+    t.text "message"
+    t.string "submission_type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sms_logs_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
@@ -29,4 +39,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_05_173734) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "sms_logs", "users"
 end
