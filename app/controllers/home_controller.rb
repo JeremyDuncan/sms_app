c59da1a01c0ac2d53375b89d6baa0fe477ce8f53
JeremyDuncan <jduncan46@my.devry.edu>
2024-07-05 14:24:15 -0400

    added routes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,10 +3,11 @@ class HomeController < ApplicationController
 
   def index
   end
-end
+
   def send_sms_button
     user = current_user
     if user
+      ActionCable.server.broadcast("job_queue_channel", { message: "#{Time.current} - Started button-triggered SMS job for #{user.first_name} #{user.last_name} - Phone Number: #{user.phone_number}" })
       SmsButtonJob.set(wait: SmsNotificationService.next_delay).perform_later(user)
     end
   end
@@ -18,6 +19,7 @@ end
 
     if user && phone_number.present? && message.present?
       ActionCable.server.broadcast("job_queue_channel", { message: "#{Time.current} - Started Custom SMS job for #{user.first_name} #{user.last_name} - Phone Number: #{user.phone_number}" })
+      SmsCustomJob.set(wait: SmsNotificationService.next_delay).perform_later(user, phone_number, message)
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'home/index'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  #
+  post 'home/send_sms_button', to: 'home#send_sms_button'
+  post 'home/send_custom_sms', to: 'home#send_custom_sms'
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
