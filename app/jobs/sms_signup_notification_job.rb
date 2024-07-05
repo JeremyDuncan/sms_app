55091624710aea4d7bd64cf6c797af33ea1781df
JeremyDuncan <jduncan46@my.devry.edu>
2024-07-05 15:53:00 -0400

    added logging

--- app/jobs/sms_signup_notification_job.rb
+++ app/jobs/sms_signup_notification_job.rb
@@ -6,5 +6,6 @@ class SmsSignupNotificationJob < ApplicationJob
 
   def perform(user)
     SmsNotificationService.send_welcome_sms(user)
+    SmsLog.create(user: user, phone_number: user.phone_number, message: "Welcome to Jeremy's Rails App, #{user.first_name} #{user.last_name}!", submission_type: "Sign Up")
   end
 end
