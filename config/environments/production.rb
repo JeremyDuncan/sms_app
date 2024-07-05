c2e4965a9279663cef24407cf684f31a61699537
JeremyDuncan <jduncan46@my.devry.edu>
2024-07-05 14:17:14 -0400

    set to use :async

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,6 +70,9 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter = :resque
   # config.active_job.queue_name_prefix = "sms_app_production"
+  # default queue adapter
+  config.active_job.queue_adapter = :async
+
 
   config.action_mailer.perform_caching = false
 
