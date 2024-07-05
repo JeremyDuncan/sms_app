1934e46b9ce6d951a6680049ef2e7bc74f01f510
JeremyDuncan <jduncan46@my.devry.edu>
2024-07-05 14:05:07 -0400

    added validations

--- app/models/sms_log.rb
+++ app/models/sms_log.rb
@@ -1,3 +1,5 @@
 class SmsLog < ApplicationRecord
   belongs_to :user
+  validates :message, presence: true
+  validates :phone_number, presence: true, format: { with: /\A(\d{10}|\d{3}-\d{3}-\d{4})\z/, message: "must be in the format 1234567890 or 123-456-7890" }
 end
