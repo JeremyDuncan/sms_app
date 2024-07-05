d0b29940036163310844b601bb7f61df59c16039
JeremyDuncan <jduncan46@my.devry.edu>
2024-07-05 14:03:37 -0400

    added validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :phone_number, presence: true, format: { with: /\A(\d{10}|\d{3}-\d{3}-\d{4})\z/, message: "must be in the format 1234567890 or 123-456-7890" }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :encrypted_password, presence: true
+
 end
