d2c6e57c4eac96507ecb7a8687c81fe1ff2e3b5e
JeremyDuncan <jduncan46@my.devry.edu>
2024-07-05 14:23:05 -0400

    added endpoints to home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,3 +4,20 @@ class HomeController < ApplicationController
   def index
   end
 end
+  def send_sms_button
+    user = current_user
+    if user
+      SmsButtonJob.set(wait: SmsNotificationService.next_delay).perform_later(user)
+    end
+  end
+
+  def send_custom_sms
+    user = current_user
+    phone_number = params[:phone_number]
+    message = params[:message]
+
+    if user && phone_number.present? && message.present?
+      ActionCable.server.broadcast("job_queue_channel", { message: "#{Time.current} - Started Custom SMS job for #{user.first_name} #{user.last_name} - Phone Number: #{user.phone_number}" })
+    end
+  end
+end
