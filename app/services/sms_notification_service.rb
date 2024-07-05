f2d5470b5f911b1eb5f33a88150638855615abad
JeremyDuncan <jduncan46@my.devry.edu>
2024-07-05 14:12:47 -0400

    updated to print easy to read console logs messages

--- app/services/sms_notification_service.rb
+++ app/services/sms_notification_service.rb
@@ -28,11 +28,29 @@ module SmsNotificationService
     from_number = twilio_config[:phone_number]
 
     client = Twilio::REST::Client.new(account_sid, auth_token)
-    client.messages.create(
+    message = client.messages.create(
       body: message_body,
       from: from_number,
       to: phone_number
     )
+    print_status(user, message)
+  end
+
+  def print_status(user, message)
+    print_header(msg: "Twilio Message Status", color: :magenta, bg_color: :none )
+    print_colored_message(msg: "Message: #{message.body}", color: :magenta, marked: false)
+    print_colored_message(msg: "From: #{user.first_name} #{user.first_name} - #{message.from}", color: :magenta, marked: false)
+    print_colored_message(msg: "To: #{message.to}", color: :magenta, marked: false)
+    print_colored_message(msg: "Date Created: #{message.date_created}", color: :magenta, marked: false)
+    print_colored_message(msg: "status: #{message.status}", color: :magenta, marked: false)
+
+    if message.error_code.present? || message.error_message.present?
+      print_colored_message(msg: "Error Code: #{message.error_code}", color: :red, marked: false)
+      print_colored_message(msg: "error_message: #{message.error_message}", color: :red, marked: false)
+    end
+    print_colored_line(color: :magenta, marked: false)
+    Rails.logger.info "Sent message to #{message.to}: #{message.sid}"
+    puts ""
   end
 
 end
